721a975edf659c9d181d22efeee2d83e22a7ac20
Yeasin <yeasin30112001@gmail.com>
2023-12-05 14:22:15 +0600

    updated

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.get('*', (__, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
 })
